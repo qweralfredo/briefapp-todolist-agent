e238b07eacd86e46c0889d078660aea647fbaf42
Alfredo Prates <qwer.alfredo@gmail.com>
2026-05-31 18:07:06 -0300

    Clean up logs, scratch files, and deploy scripts

--- scratch.py
+++
@@ -1,8 +0,0 @@
-import json
-import re
-import urllib.request
-import urllib.parse
-from http.client import HTTPConnection
-
-# Let's just create a list of ids and then I can call the MCP tool, but since I can't call MCP from python, 
-# I will just write a PS script or simply tell the user the backlog is done.
